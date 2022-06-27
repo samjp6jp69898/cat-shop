2a3d50f41e15dc45cba761f8d6ef405fedfdf929
unknown <samjp6jp69898@gmail.com>
2022-06-27 23:01:08 +0800

    菜單更新

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ import ig from "./logo/instagram.png";
 import line2 from "./logo/line2.png";
 import catbg from "./logo/貓咪背景圖.png";
 import { useState } from "react";
+import { navitem } from "./navitems";
 
 function App() {
   const [showinfo, setShowinfo] = useState("");
   const [showcart, setShowcart] = useState("");
   const [showorder, setShoworder] = useState("");
-  const [popup, setPopup] = useState(false);
 
   const displayinfo = () => {
     setShowinfo("購物前須知");
@@ -40,63 +40,64 @@ function App() {
     setShoworder("");
   };
 
-  const showpopup = () => setPopup(!popup);
-
   return (
     <div className="App">
       <div className="container">
         <div className="navbar">
-          <i class="fa-solid fa-bars" onClick={showpopup}></i>
-          <div className={popup ? "popmenu active" : "popmenu"} id="popup">
-            <i class="fa-solid fa-xmark" onClick={showpopup}></i>
+          <div className="nav-items">
+            <h2 className="nav-item">貓罐罐</h2>
+            <h2 className="nav-item">貓罐罐</h2>
           </div>
-          <div className="nav info">
-            <img
-              src={info}
-              alt=""
-              className="icon-info"
-              onMouseOver={displayinfo}
-              onMouseOut={mouseLeaveinfo}
-            ></img>
-            <p className="infoP">{showinfo}</p>
-          </div>
-          <div className="nav cart">
-            <img
-              src={cart}
-              alt=""
-              className="icon-cart"
-              onMouseOver={displaycart}
-              onMouseOut={mouseLeavecart}
-            ></img>
-            <p className="infoC">{showcart}</p>
-          </div>
-          <div className="nav order">
-            <img
-              src={order}
-              alt=""
-              className="icon-order"
-              onMouseOver={displayorder}
-              onMouseOut={mouseLeaveorder}
-            ></img>
-            <p className="infoO">{showorder}</p>
-          </div>
+          <div className="nav-wrapper">
+            <div className="nav info">
+              <img
+                src={info}
+                alt=""
+                className="icon-info"
+                onMouseOver={displayinfo}
+                onMouseOut={mouseLeaveinfo}
+              ></img>
+              <p className="infoP">{showinfo}</p>
+            </div>
+            <div className="nav cart">
+              <img
+                src={cart}
+                alt=""
+                className="icon-cart"
+                onMouseOver={displaycart}
+                onMouseOut={mouseLeavecart}
+              ></img>
+              <p className="infoC">{showcart}</p>
+            </div>
+            <div className="nav order">
+              <img
+                src={order}
+                alt=""
+                className="icon-order"
+                onMouseOver={displayorder}
+                onMouseOut={mouseLeaveorder}
+              ></img>
+              <p className="infoO">{showorder}</p>
+            </div>
 
-          <div className="nav-logo-container fb">
-            <img src={fb} alt=""></img>
-            <p>登入</p>
+            <div className="nav-logo-container fb">
+              <img src={fb} alt=""></img>
+              <p>登入</p>
+            </div>
+            <div className="nav-logo-container google">
+              <img src={google} alt=""></img>
+              <p>登入</p>
+            </div>
+            <div className="nav-logo-container line">
+              <img src={line} alt=""></img>
+              <p>登入</p>
+            </div>
+            <img src={ig} alt="" className="contact-icon-ig"></img>
+            <img src={line2} alt="" className="contact-icon-line"></img>
+            <img src={fb} alt="" className="contact-icon-fb"></img>
           </div>
-          <div className="nav-logo-container google">
-            <img src={google} alt=""></img>
-            <p>登入</p>
-          </div>
-          <div className="nav-logo-container line">
-            <img src={line} alt=""></img>
-            <p>登入</p>
-          </div>
-          <img src={ig} alt="" className="contact-icon-ig"></img>
-          <img src={line2} alt="" className="contact-icon-line"></img>
-          <img src={fb} alt="" className="contact-icon-fb"></img>
         </div>
+
         <div className="hero_container">
           <img src={catbg} alt=""></img>
         </div>
---
+++ src/navitems.js
@@ -0,0 +1,49 @@
+export const navitem= [
+ {
+    id: 1,
+    title: "貓罐頭"
+ },
+ {
+    id: 2,
+    title: "貓飼料"
+ },
+ {
+    id: 3,
+    title: "貓零食/肉泥"
+ },
+ {
+    id: 4,
+    title: "貓咪居家用品"
+ },
+ {
+    id: 4,
+    title: "貓咪玩具"
+ },
+]
+
+export const can = [
+   {
+      id: 1,
+      title: "貓主食罐"
+   },
+   {
+      id: 2,
+      title: "貓副食"
+   },
+   {
+      id: 3,
+      title: "湯罐"
+   },
+   {
+      id: 4,
+      title: "整箱組合"
+   },
+   {
+      id: 5,
+      title: "老貓系列"
+   },
+   {
+      id: 6,
+      title: "幼貓系列"
+   },
+]
